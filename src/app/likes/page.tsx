a50a85e2bcefa6a7024f6d31282e1d10457fdfb6
Denys Paziak <67861412+Denys-Paziak@users.noreply.github.com>
2023-12-21 05:23:19 +0200

    ssfixф

--- src/app/likes/page.tsx
+++ src/app/likes/page.tsx
@@ -1,18 +1,19 @@
 "use client";
-
 import data from "../product_data.json";
-
 import { v4 as uuidv4 } from 'uuid';
 
 interface Product {
-    id: number; // Assuming 'id' is of number type, adjust as needed
+    id: number;
+    images: string[]; // or adjust according to your data structure
+    name: string;
+    price: string;
     // Add other properties of your 'el' object here
 }
 
 const Likes = () => {
-
-    let storedProducts: number[] = JSON.parse(localStorage.getItem('products')) || [];
-    let products: Product[] = [];
+    const storedProductsJSON = localStorage.getItem('products');
+    const storedProducts: number[] = storedProductsJSON ? JSON.parse(storedProductsJSON) : [];
+    const products: Product[] = [];
 
     data.forEach((el: Product) => {
         if (storedProducts.includes(el.id)) {
@@ -20,27 +21,21 @@ const Likes = () => {
         }
     });
 
-
     return (
         <div className="min-h-[80vh]">
             <div className='container mx-auto grid grid-cols-2 gap-6 py-6'>
-                {products.map(el => {
-                    return <a key={uuidv4()} href={`/${el.id}`} className="flex items-center gap-2">
+                {products.map(el => (
+                    <a key={uuidv4()} href={`/${el.id}`} className="flex items-center gap-2">
                         <img className="w-[50px] h-[50px] object-cover" src={el.images[0]} alt="" />
                         <div>
-                            <p>
-                                {el.name}
-                            </p>
-                            <p className="font-bold">
-                                {el.price}
-                            </p>
+                            <p>{el.name}</p>
+                            <p className="font-bold">{el.price}</p>
                         </div>
                     </a>
-                })}
+                ))}
             </div>
         </div>
-    )
+    );
 }
 
-
-export default Likes
+export default Likes;
